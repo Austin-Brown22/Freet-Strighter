09f958267a5f3266bc87b2822c6f1d0fd54d719a
Austin-Brown22 <austinmbrown22@gmail.com>
2018-04-02 13:38:43 -0500

    got background working

--- Runner.py
+++ Runner.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
-background = "path\something"#the file path of the background image
-win_size = (800,800)#window size
+background = 'C:/Users/b0503447/PycharmProjects/Freet-Strighter/stage.jpg'#the file path of the background image
+win_size = (1500,480)#window size
 pygame.init()
 screen = pygame.display.set_mode(win_size,0,32)
 pygame.display.set_caption("Freet Strighter")
