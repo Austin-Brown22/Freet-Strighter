be98c599fb765715eb65ae70fd8cf82929cf308c
Austin-Brown22 <austinmbrown22@gmail.com>
2018-04-04 13:41:14 -0500

    continued on players

--- Runner.py
+++ Runner.py
@@ -1,5 +1,8 @@
 import pygame
 import sys
+import players
+
+
 background = 'stage.jpg'  # the file path of the background image||except it understands if in same folder
 win_size = (1500,480)  # window size
 pygame.init()
@@ -11,16 +14,16 @@ screen.blit(pygame.image.load(background), (0, 0))  # loading background
 
 # create players
 player_group = pygame.sprite.Group()
-player_one = pygame.sprite.Sprite()
-player_two = pygame.sprite.Sprite()
-player_group.add(player_one,player_two)
+player_one = players.Players(100, 100, 5,'ryu')  # ryu
+player_two = players.Players(500, 100, 5, 'ken')  # ken
+player_group.add(player_one, player_two)
 
 running = True
 while running:
     for evt in pygame.event.get():
         if evt.type == pygame.QUIT:
             running = False
-
+    player_group.update()
     pygame.display.update()
 pygame.quit()
 sys.exit()
--- players.py
+++ players.py
@@ -2,21 +2,27 @@ import pygame
 import sys
 
 
-class Players:
-    image = pygame.image.load('ryu_idle1.png')  # temp filename till wess gets his shit together
+class Players(pygame.sprite.Sprite):
 
-    def __init__(self, xinit, yintit, speed):
+    def __init__(self, xinit, yintit, speed, nam):
         # pygame.sprite.Sprite.__init__(self)
+        self._Sprite__g = []
+        self.name = nam
+        self.image = pygame.image.load(self.name + '_idle1.png')
         self.rect = self.image.get_rect()
         self.rect.x = xinit
         self.rect.y = yintit
         self.vel = speed
 
+    def update(self):#need to make this work before i can actually test anything
+        pass
+
     # change image and update self.rect
     def crouch(self):
-        image = pygame.image.load('ryu_crouch.png')
+        self.image = pygame.image.load('ryu_crouch.png')
         self.rect = self.image.get_rect()
 
+    # go through jump animation and du de stuph
     def jump(self):
         pass
 
@@ -29,3 +35,6 @@ class Players:
             self.crouch()
         elif direction == 'up':
             self.jump()
+
+
+
