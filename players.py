51cad98992b705356c95b5eaebde47d04c055005
b0503367 <32579347+b0503367@users.noreply.github.com>
2018-04-04 12:50:09 -0500

    Update players.py

--- players.py
+++ players.py
@@ -3,7 +3,7 @@ import sys
 
 
 class Players:
-    image = pygame.image.load('ryu_idle1.jpeg')  # temp filename till wess gets his shit together
+    image = pygame.image.load('ryu_idle1.png')  # temp filename till wess gets his shit together
 
     def __init__(self, xinit, yintit, speed):
         # pygame.sprite.Sprite.__init__(self)
@@ -14,7 +14,7 @@ class Players:
 
     # change image and update self.rect
     def crouch(self):
-        image = pygame.image.load('ryu_crouch.jpeg')
+        image = pygame.image.load('ryu_crouch.png')
         self.rect = self.image.get_rect()
 
     def jump(self):
