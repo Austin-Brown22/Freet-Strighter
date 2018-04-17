e270fe6db9915aed45d19e3c989126cc66620b10
Austin-Brown22 <austinmbrown22@gmail.com>
2018-04-17 13:49:38 -0500

    continuing to jump

--- players.py
+++ players.py
@@ -50,10 +50,10 @@ class Players(pygame.sprite.Sprite):
     def jump(self):
         self.threads.append(thread_animations.Thread_Animations('thread' + str(len(self.threads)), 'jump', self, 2, .2))
         self.threads[-1].start()
-        self.threads.append(thread_moving_sprites.thread_Moving_Sprites('thread' + str(len(self.threads)),50,50,1,self))
+        self.threads.append(thread_moving_sprites.thread_Moving_Sprites('thread' + str(len(self.threads)),50,-50,1,self))
         self.threads[-1].start()
-        # do a .join so that the secound move tghread starts after the first move thread
-        self.threads.append(thread_moving_sprites.thread_Moving_Sprites('thread' + str(len(self.threads)), 50, -50, 1, self, self.threads[-1]))
+        # do a .join so that the secound move tghread starts after the first move thread or dont
+        self.threads.append(thread_moving_sprites.thread_Moving_Sprites('thread' + str(len(self.threads)), 50, 50, 1, self, self.threads[-1]))
         self.threads[-1].start()
 
     def move(self, direction):
--- thread_moving_sprites.py
+++ thread_moving_sprites.py
@@ -6,14 +6,25 @@ class thread_Moving_Sprites(threading.Thread):
 
     def __init__(self, name, dX, dY, period, pler, joined=None):
         threading.Thread.__init__(self, target=self.run)
-        self.deltaX = dX
-        self.deltaY = dY
-        self.time = period
+        self.x_vel = (dX)/period
+        self.y_vel = dY/period
+        self.period = period
         self.name = name
         self.player = pler
         self.to_join = joined
 
 
+
     def run(self):
         if not self.to_join == None:
+            print('join')
             self.to_join.join()
+
+        print('start')
+        for i in range(self.period):
+            print(self.player.rect.y)
+            self.player.rect.x += self.x_vel
+            self.player.rect.y += self.y_vel
+            print(self.player.rect.y)
+            time.sleep(.3)
+        print('end')
