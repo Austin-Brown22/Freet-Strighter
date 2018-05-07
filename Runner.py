7e3db733b3561c79b9b25f9d447229bc14a2ef32
Austin-Brown22 <austinmbrown22@gmail.com>
2018-05-07 13:46:22 -0500

    starting the end

--- Runner.py
+++ Runner.py
@@ -19,6 +19,7 @@ colliding = False
 ryu_hitbox_cord = (0,0)
 ken_hitbox_cord = (0,0)
 has_perried = False
+done = False
 
 
 screen.blit(pygame.image.load(background), (0, 0))  # loading background
@@ -140,107 +141,112 @@ while running:
         'jumppunch1':  (77,158,55,54),
         'jumpkick1':   (82,215,61,42),
     }
-    #SPRITES.COLLIDE_MASK() R-hitsprite and k-player
-    colliding = False if pygame.sprite.collide_mask(player_one,player_two) is None else True
-    #make a collide variable in plar set it here
-    player_two.is_colliding = colliding
-    player_one.is_colliding = colliding
-    if player_one.image_name in frame_hurtbox_Ryu and frame_hurtbox_Ryu[player_one.image_name] is not None:
-        ryu_hitbox = pygame.Surface((frame_hurtbox_Ryu[player_one.image_name][2],frame_hurtbox_Ryu[player_one.image_name][3]))
-        ryu_hitbox_cord = (frame_hurtbox_Ryu[player_one.image_name][0] + player_one.rect.x, frame_hurtbox_Ryu[player_one.image_name][1] + player_one.rect.y)
-    else:
-        ryu_hitbox = None
-        ryu_hitbox_cord = None
-    if player_two.image_name in frame_hurtbox_ken and frame_hurtbox_ken[player_two.image_name] is not None:
-        ken_hitbox = pygame.Surface((frame_hurtbox_ken[player_two.image_name][2],frame_hurtbox_ken[player_two.image_name][3]))
-        ken_hitbox_cord = (frame_hurtbox_ken[player_two.image_name][0] + player_two.rect.x, frame_hurtbox_ken[player_two.image_name][1] + player_two.rect.y)
-    else:
-        ken_hitbox = None
-        ken_hitbox_cord = None
-    #check actual collisions of hitbox masks
-    if ken_hitbox is not None:
-        ken_mask = pygame.mask.from_surface(ken_hitbox)
-        ken_mask.fill()
-    else:
-        ken_mask = None
-    if ryu_hitbox is not None:
-        ryu_mask = pygame.mask.from_surface(ryu_hitbox)
-        ryu_mask.fill()
-    else:
-        ryu_mask = None
-    #parry
-    if (ken_mask is not None) and (ryu_mask is not None) and (ken_mask.overlap(ryu_mask,(ryu_hitbox_cord[0]-ken_hitbox_cord[0],ryu_hitbox_cord[1]-ken_hitbox_cord[1])) is not None):
-        # a parry has happened
-        # do hurt animation for both
-        # no damage
-        has_perried = True
-        for frame in player_one.threads + player_two.threads:
-            if re.search('.*(punch|kick).*',frame.action):
-                print("canceling")
-                frame.player.shtahp_thread = True
-        ryu_hurt_animationP = thread_animations.Thread_Animations('ryu_hurt_thread', 'hit', player_one, 1, .25,hold_time=.2)
-        ryu_hurt_animationP.start()
-        ryu_hit_slideP = thread_moving_sprites.thread_Moving_Sprites('ryu_slide_thread', 30, 0, 1, player_one)
-        ryu_hit_slideP.start()
-        ken_hurt_animationP = thread_animations.Thread_Animations('ken_hurt_thread', 'hit', player_two, 1, .25,hold_time=.2)
-        ken_hurt_animationP.start()
-        ken_hit_slideP = thread_moving_sprites.thread_Moving_Sprites('ken_slide_thread', -30, 0, 1, player_two)
-        ken_hit_slideP.start()
-    else:
-        has_perried = False
-    #check for contact to sprite
-    ken_urt_box = pygame.mask.from_surface(player_two.image)
-    ryu_urt_box = pygame.mask.from_surface(player_one.image)
-    if not has_perried and ken_mask is not None and not player_one.image_name == 'hit1':
-        if ken_mask.overlap(ryu_urt_box, (player_one.rect.x - ken_hitbox_cord[0], player_one.rect.y - ken_hitbox_cord[1])) is not None:
-            print('ken lands a hit')
-            # do hurt animation for person hit
-            #   do animation and move them back some to avoid spam
-            # damage one
-            player_two.cur_health -= 1
-            print(percur2)
-            percur1 = player_one.cur_health * 100 / player_one.max_health
-            percur2 = player_two.cur_health * 100 / player_two.max_health
-            ryu_hurt_animation = thread_animations.Thread_Animations('ryu_hurt_thread', 'hit', player_one, 1, .25)
-            ryu_hurt_animation.start()
-            ryu_hit_slide = thread_moving_sprites.thread_Moving_Sprites('ryu_slide_thread', 30, 0, 1, player_one)
-            ryu_hit_slide.start()
-    if not has_perried and ryu_mask is not None and not player_two.image_name == 'hit1':
-        if ryu_mask.overlap(ken_urt_box, (player_two.rect.x - ryu_hitbox_cord[0], player_two.rect.y - ryu_hitbox_cord[1])) is not None:
-            print('ryu lands a hit')
-            # do hurt animation
-            # damage one
-            player_one.cur_health -= 1
-            print(percur1)
-            percur1 = player_one.cur_health * 100 / player_one.max_health
-            percur2 = player_two.cur_health * 100 / player_two.max_health
-            ken_hurt_animation = thread_animations.Thread_Animations('ken_hurt_thread', 'hit', player_two, 1, .25)
-            ken_hurt_animation.start()
-            ken_hit_slide = thread_moving_sprites.thread_Moving_Sprites('ken_slide_thread', -30, 0, 1, player_two)
-            ken_hit_slide.start()
-    #End MASK SECTION
+    if not done:
+        #SPRITES.COLLIDE_MASK() R-hitsprite and k-player
+        colliding = False if pygame.sprite.collide_mask(player_one,player_two) is None else True
+        #make a collide variable in plar set it here
+        player_two.is_colliding = colliding
+        player_one.is_colliding = colliding
+        if player_one.image_name in frame_hurtbox_Ryu and frame_hurtbox_Ryu[player_one.image_name] is not None:
+            ryu_hitbox = pygame.Surface((frame_hurtbox_Ryu[player_one.image_name][2],frame_hurtbox_Ryu[player_one.image_name][3]))
+            ryu_hitbox_cord = (frame_hurtbox_Ryu[player_one.image_name][0] + player_one.rect.x, frame_hurtbox_Ryu[player_one.image_name][1] + player_one.rect.y)
+        else:
+            ryu_hitbox = None
+            ryu_hitbox_cord = None
+        if player_two.image_name in frame_hurtbox_ken and frame_hurtbox_ken[player_two.image_name] is not None:
+            ken_hitbox = pygame.Surface((frame_hurtbox_ken[player_two.image_name][2],frame_hurtbox_ken[player_two.image_name][3]))
+            ken_hitbox_cord = (frame_hurtbox_ken[player_two.image_name][0] + player_two.rect.x, frame_hurtbox_ken[player_two.image_name][1] + player_two.rect.y)
+        else:
+            ken_hitbox = None
+            ken_hitbox_cord = None
+        #check actual collisions of hitbox masks
+        if ken_hitbox is not None:
+            ken_mask = pygame.mask.from_surface(ken_hitbox)
+            ken_mask.fill()
+        else:
+            ken_mask = None
+        if ryu_hitbox is not None:
+            ryu_mask = pygame.mask.from_surface(ryu_hitbox)
+            ryu_mask.fill()
+        else:
+            ryu_mask = None
+        #parry
+        if (ken_mask is not None) and (ryu_mask is not None) and (ken_mask.overlap(ryu_mask,(ryu_hitbox_cord[0]-ken_hitbox_cord[0],ryu_hitbox_cord[1]-ken_hitbox_cord[1])) is not None):
+            # a parry has happened
+            # do hurt animation for both
+            # no damage
+            has_perried = True
+            for frame in player_one.threads + player_two.threads:
+                if re.search('.*(punch|kick).*',frame.action):
+                    print("canceling")
+                    frame.player.shtahp_thread = True
+            ryu_hurt_animationP = thread_animations.Thread_Animations('ryu_hurt_thread', 'hit', player_one, 1, .25,hold_time=.2)
+            ryu_hurt_animationP.start()
+            ryu_hit_slideP = thread_moving_sprites.thread_Moving_Sprites('ryu_slide_thread', 30, 0, 1, player_one)
+            ryu_hit_slideP.start()
+            ken_hurt_animationP = thread_animations.Thread_Animations('ken_hurt_thread', 'hit', player_two, 1, .25,hold_time=.2)
+            ken_hurt_animationP.start()
+            ken_hit_slideP = thread_moving_sprites.thread_Moving_Sprites('ken_slide_thread', -30, 0, 1, player_two)
+            ken_hit_slideP.start()
+        else:
+            has_perried = False
+        #check for contact to sprite
+        ken_urt_box = pygame.mask.from_surface(player_two.image)
+        ryu_urt_box = pygame.mask.from_surface(player_one.image)
+        if not has_perried and ken_mask is not None and not player_one.image_name == 'hit1':
+            if ken_mask.overlap(ryu_urt_box, (player_one.rect.x - ken_hitbox_cord[0], player_one.rect.y - ken_hitbox_cord[1])) is not None:
+                print('ken lands a hit')
+                # do hurt animation for person hit
+                #   do animation and move them back some to avoid spam
+                # damage one
+                player_two.cur_health -= 1
+                print(percur2)
+                percur1 = player_one.cur_health * 100 / player_one.max_health
+                percur2 = player_two.cur_health * 100 / player_two.max_health
+                ryu_hurt_animation = thread_animations.Thread_Animations('ryu_hurt_thread', 'hit', player_one, 1, .25)
+                ryu_hurt_animation.start()
+                ryu_hit_slide = thread_moving_sprites.thread_Moving_Sprites('ryu_slide_thread', 30, 0, 1, player_one)
+                ryu_hit_slide.start()
+        if not has_perried and ryu_mask is not None and not player_two.image_name == 'hit1':
+            if ryu_mask.overlap(ken_urt_box, (player_two.rect.x - ryu_hitbox_cord[0], player_two.rect.y - ryu_hitbox_cord[1])) is not None:
+                print('ryu lands a hit')
+                # do hurt animation
+                # damage one
+                player_one.cur_health -= 1
+                print(percur1)
+                percur1 = player_one.cur_health * 100 / player_one.max_health
+                percur2 = player_two.cur_health * 100 / player_two.max_health
+                ken_hurt_animation = thread_animations.Thread_Animations('ken_hurt_thread', 'hit', player_two, 1, .25)
+                ken_hurt_animation.start()
+                ken_hit_slide = thread_moving_sprites.thread_Moving_Sprites('ken_slide_thread', -30, 0, 1, player_two)
+                ken_hit_slide.start()
+        #End MASK SECTION
 
-
-    for pressed in keys:
-        if keys[pressed]:
-            player_group.move(pressed)
+    if not done:
+        for pressed in keys:
+            if keys[pressed]:
+                player_group.move(pressed)
     screen.blit(pygame.image.load(background), (0, 0))
     player_group.draw(screen)
     name_group.draw(screen)
-    if ken_hitbox_cord is not None:
-        mask_size = ken_mask.get_size()
-        pygame.draw.rect(screen, color_red, (ken_hitbox_cord[0], ken_hitbox_cord[1], mask_size[0], mask_size[1]))
 
-    if ryu_hitbox_cord is not None:
-        mask_size = ryu_mask.get_size()
-        pygame.draw.rect(screen, color_red, (ryu_hitbox_cord[0], ryu_hitbox_cord[1], mask_size[0], mask_size[1]))
+    #if ken_hitbox_cord is not None:
+     #   mask_size = ken_mask.get_size()
+      #  pygame.draw.rect(screen, color_red, (ken_hitbox_cord[0], ken_hitbox_cord[1], mask_size[0], mask_size[1]))
 
+#    if ryu_hitbox_cord is not None:
+ #       mask_size = ryu_mask.get_size()
+  #      pygame.draw.rect(screen, color_red, (ryu_hitbox_cord[0], ryu_hitbox_cord[1], mask_size[0], mask_size[1]))
+#
     for player in player_group.sprites():
         pygame.draw.rect(screen, color_white, (23, 23, 503, 28), 2)
         pygame.draw.rect(screen, color_white, (973, 23, 503, 28), 2)
-
         pygame.draw.rect(screen, color_red,(25,25,percur1 * 5,25))
         pygame.draw.rect(screen,color_red,(975,25,percur2 * 5,25))
+
+    if player_one.cur_health <= 0 or player_one.cur_health <= 0:
+        done = True
+
 
 
 
